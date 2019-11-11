811a266b7a579aae7cd50a19976da9105a26870a
Ramji Balasubramanian <ramji.b28@gmail.com>
2019-11-11 18:22:01 +0530

    Update testdata.py

--- testdata.py
+++ testdata.py
@@ -30,8 +30,8 @@ def moving_files(df,t,mode='test'):
         plt.imsave(dest,img)
     bar.finish()
 
-t1 = threading.Thread(target=moving_files, args=(test_df[0:int(test_df.shape[0]/10)],'t1',)) 
-t2 = threading.Thread(target=moving_files, args=(test_df[int(test_df.shape[0]/10):2*int(test_df.shape[0]/10)],'t2',)) 
+t1 = threading.Thread(target=moving_files, args=(test_df[0:int(test_df.shape[0]/10)],'t1',))
+t2 = threading.Thread(target=moving_files, args=(test_df[int(test_df.shape[0]/10):2*int(test_df.shape[0]/10)],'t2',))
 t3 = threading.Thread(target=moving_files, args=(test_df[2*int(test_df.shape[0]/10):3*int(test_df.shape[0]/10)],'t3',))
 t4 = threading.Thread(target=moving_files, args=(test_df[3*int(test_df.shape[0]/10):4*int(test_df.shape[0]/10)],'t4',))
 t5 = threading.Thread(target=moving_files, args=(test_df[4*int(test_df.shape[0]/10):5*int(test_df.shape[0]/10)],'t5',))
@@ -40,21 +40,21 @@ t7 = threading.Thread(target=moving_files, args=(test_df[6*int(test_df.shape[0]/
 t8 = threading.Thread(target=moving_files, args=(test_df[7*int(test_df.shape[0]/10):8*int(test_df.shape[0]/10)],'t8',))
 t9 = threading.Thread(target=moving_files, args=(test_df[8*int(test_df.shape[0]/10):9*int(test_df.shape[0]/10)],'t9',))
 t10 = threading.Thread(target=moving_files, args=(test_df[9*int(test_df.shape[0]/10):],'t10',))
-t1.start() 
-# starting thread 2 
+t1.start()
+# starting thread 2
 t2.start()
 t3.start()
 t4.start()
 t5.start()
 t6.start()
 t7.start()
-t8.start() 
+t8.start()
 t9.start()
 t10.start()
-# wait until thread 1 is completely executed 
-t1.join() 
-# wait until thread 2 is completely executed 
-t2.join() 
+# wait until thread 1 is completely executed
+t1.join()
+# wait until thread 2 is completely executed
+t2.join()
 t3.join()
 t4.join()
 t5.join()
